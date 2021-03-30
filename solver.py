2b8023e125fffcae0cb99c8391aac4f46b85e087
kanido386 <kanido386@gmail.com>
2021-03-30 15:48:59 +0800

    Add new puzzle L00.txt

--- solver.py
+++ solver.py
@@ -13,7 +13,7 @@ class Solver(object):
 
     # Ask user which level to play with
     print('===================================')
-    print('有 01~04, 10~11, 20~31, 40 這些關卡')
+    print('有 00~04, 10~11, 20~31, 40 這些關卡')
     print('===================================')
     puzzle_name = input('請輸入您想要解決的關卡：')
     
